5660a0ba43d5457a907f3c189ff5aef16a8b5118
skunka <andrius@andajus.com>
2011-05-10 00:12:02 +0300

    jquery fixai del heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -46,4 +46,6 @@ Laspa::Application.configure do
 
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
+  
+  ACTIVE_SCAFFOLD_INSTALL_ASSETS = false
 end
